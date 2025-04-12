24ff9e57aca37212df3eeecd2dfc927275713c06
pinklovee10 <102283498+pinklovee10@users.noreply.github.com>
2025-04-13 05:59:29 +0700

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
 const PORT = process.env.PORT || 3000;
-require("dotenv").config();
 
 app.use(express.json());
 
